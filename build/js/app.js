eb94a4a5c9b48b7dbde8a33f1e885af995196e39
Danyygtz <daniela.gtz.encinas@gmail.com>
2025-06-20 16:47:14 -0600

    Actualiza proyecto con mejoras en imágenes y estructura

--- build/js/app.js
+++ build/js/app.js
@@ -25,10 +25,12 @@ function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
 
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galeria'
-
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
+        
         //Event Handler
         imagen.onclick = function(){
             mostrarImagen(i)
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
+import path from 'path';
+import fs from 'fs';
+import { glob } from 'glob';
 import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
+
+import terser from 'gulp-terser';
+import sharp from 'sharp';
 
 export function js( done ){
     src('src/js/app.js')
@@ -19,9 +25,77 @@ export function css( done ) {
     done()
 }
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export async function imagenes(done) {
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    // const images =  await glob('./src/img/**/*{jpg,png}')
+    const images = await glob('./src/img/**/*.{jpg,png}', {
+                    ignore: ['./src/img/gallery/thumb/**']
+                    });
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
+    done();
+}
+
+async function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+
+    const options = { quality: 80 }
+    
+    try {
+        await sharp(file).jpeg(options).toFile(outputFile);
+        await sharp(file).webp(options).toFile(outputFileWebp);
+        await sharp(file).avif(options).toFile(outputFileAvif);
+    } catch (error) {
+        console.error(`Error procesando ${file}:`, error.message);
+    }
+
+}
+
 export function dev(){
     watch('src/scss/**/*.scss', css)
     watch('src/js/**/*.js', js)
+    watch('src/img/**/*.{jpg,png}', imagenes)
 }
 
-export default series( js, css, dev )
+export default series( crop, js, css, imagenes, dev )
